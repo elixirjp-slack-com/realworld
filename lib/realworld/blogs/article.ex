6c539b2d7037fad66022f0f916b354d46fa5dcb1
TORIFUKUKaiou <torifuku.kaiou@gmail.com>
2021-11-30 22:08:53 +0900

    article has_many comments

--- lib/realworld/blogs/article.ex
+++ lib/realworld/blogs/article.ex
@@ -2,9 +2,12 @@ defmodule Realworld.Blogs.Article do
   use Ecto.Schema
   import Ecto.Changeset
 
+  alias Realworld.Blogs.Comment
+
   schema "articles" do
     field :body, :string
     field :title, :string
+    has_many :comments, Comment
 
     timestamps()
   end
