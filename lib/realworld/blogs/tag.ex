cb85d66647617e67b7680cff99ea13a384ec72c5
TORIFUKUKaiou <torifuku.kaiou@gmail.com>
2021-11-30 22:19:28 +0900

    mix phx.gen.schema Blogs.Tag tags tag:string:unique

---
+++ lib/realworld/blogs/tag.ex
@@ -0,0 +1,18 @@
+defmodule Realworld.Blogs.Tag do
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  schema "tags" do
+    field :tag, :string
+
+    timestamps()
+  end
+
+  @doc false
+  def changeset(tag, attrs) do
+    tag
+    |> cast(attrs, [:tag])
+    |> validate_required([:tag])
+    |> unique_constraint(:tag)
+  end
+end
