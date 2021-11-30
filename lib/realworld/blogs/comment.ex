07d8132c8e1915575f9a5261825e76d1a29f67d3
TORIFUKUKaiou <torifuku.kaiou@gmail.com>
2021-11-30 22:05:00 +0900

    comment belongs_to article & add null: false

--- lib/realworld/blogs/comment.ex
+++ lib/realworld/blogs/comment.ex
@@ -2,9 +2,11 @@ defmodule Realworld.Blogs.Comment do
   use Ecto.Schema
   import Ecto.Changeset
 
+  alias Realworld.Blogs.Article
+
   schema "comments" do
     field :body, :string
-    field :article_id, :id
+    belongs_to :article, Article
 
     timestamps()
   end
@@ -12,7 +14,7 @@ defmodule Realworld.Blogs.Comment do
   @doc false
   def changeset(comment, attrs) do
     comment
-    |> cast(attrs, [:body])
-    |> validate_required([:body])
+    |> cast(attrs, [:body, :article_id])
+    |> validate_required([:body, :article_id])
   end
 end
--- priv/repo/migrations/20211130130020_create_comments.exs
+++ priv/repo/migrations/20211130130020_create_comments.exs
@@ -3,8 +3,8 @@ defmodule Realworld.Repo.Migrations.CreateComments do
 
   def change do
     create table(:comments) do
-      add :body, :string
-      add :article_id, references(:articles, on_delete: :nothing)
+      add :body, :string, null: false
+      add :article_id, references(:articles, on_delete: :nothing), null: false
 
       timestamps()
     end
