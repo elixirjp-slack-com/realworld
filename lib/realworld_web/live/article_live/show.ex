aa9be4af80aed410d06ac63212987e370d8180b6
SAITO Kazuya <saito@smartalgorithm.co.jp>
2021-12-03 14:12:12 +0900

    Revise updating article

--- lib/realworld_web/live/article_live/show.ex
+++ lib/realworld_web/live/article_live/show.ex
@@ -3,6 +3,8 @@ defmodule RealworldWeb.ArticleLive.Show do
 
   alias Realworld.Blogs
 
+  on_mount RealWorldWeb.CurrentUserAssign
+
   @impl true
   def mount(_params, _session, socket) do
     {:ok, socket}
@@ -10,10 +12,25 @@ defmodule RealworldWeb.ArticleLive.Show do
 
   @impl true
   def handle_params(%{"id" => id}, _, socket) do
-    {:noreply,
-     socket
-     |> assign(:page_title, page_title(socket.assigns.live_action))
-     |> assign(:article, Blogs.get_article!(id))}
+    article = Blogs.get_article!(id)
+    %{
+      live_action: action,
+      current_user: user
+    } = socket.assigns
+    author_id = article.author_id
+
+    if action == :edit && author_id != user.id do
+      redirect_path = Routes.article_show_path(socket, :show, article)
+      {:noreply,
+       socket
+       |> put_flash(:error, "You can't edit this article")
+       |> push_redirect(to: redirect_path)}
+    else
+      {:noreply,
+       socket
+       |> assign(:page_title, page_title(socket.assigns.live_action))
+       |> assign(:article, article)}
+    end
   end
 
   defp page_title(:show), do: "Show Article"
