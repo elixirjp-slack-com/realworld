a9c3de56703102a0a30be6a9c216f8322303bd29
TORIFUKU Kaiou <torifuku.kaiou@gmail.com>
2021-12-12 11:42:05 +0900

    https://elixirjp.slack.com/archives/C02BZ4CFL8N/p1639276468031300

--- lib/realworld_web/live/current_user_assign.ex
+++ lib/realworld_web/live/current_user_assign.ex
@@ -1,7 +1,7 @@
 defmodule RealWorldWeb.CurrentUserAssign do
   import Phoenix.LiveView
 
-  def mount(_params, session, socket) do
+  def on_mount(:user, _params, session, socket) do
     token = session["user_token"] || ""
     current_user = Realworld.Accounts.get_user_by_session_token(token)
     {:cont, assign(socket, :current_user, current_user)}
